e38421ff9d28c5da6e13ce73c3ce65aaa709563e
ajcapacete <ajcapacete@gmail.com>
2024-06-02 18:30:51 +0800

    [feature/list] Extract svg images

---
+++ src/types/ProductItem.ts
@@ -0,0 +1,14 @@
+interface ProductItem {
+    category: string,
+    description: string,
+    id: number,
+    image: string,
+    price: number
+    rating: {
+        rate: number,
+        count: number,
+    },
+    title: string,
+}
+
+export default ProductItem;
